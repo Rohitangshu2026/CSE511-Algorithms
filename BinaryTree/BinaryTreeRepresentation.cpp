e0a7cf71d507ea10b7f21ae4c7e902758d632093
Rohitangshu Bose <49076701+Rohitangshu2026@users.noreply.github.com>
2025-09-02 17:52:31 +0530

    Update BinaryTreeRepresentation.cpp

--- BinaryTree/BinaryTreeRepresentation.cpp
+++ BinaryTree/BinaryTreeRepresentation.cpp
@@ -1,39 +1,36 @@
-#include <iostream>
+#include<iostream>
+using namespace std;
 
-// Structure definition for
-// a node in a binary tree
-struct Node {
-    // Defining the value of
-    // the node (integer data)
-    int data;        
-     // Reference pointer to
-     // the left child node
-    Node* left;      
-    // Reference pointer to
-    // the right child node
-    Node* right;      
+// structure definition of a node in a binary tree
+struct node
+{
+    int data;       // stores data value
+    node* left;     // stores pointer to left child
+    node* right;    // stores pointer to right child
 
-    // Method to initialize
-    // the node with a value
-    Node(int val) {
-        // Set the value of the
-        // node to the passed integer
-        data = val;   
-        // Initialize left and 
-        //r ight pointers as NULL
-        left = right = NULL;  
+    // initialize the node with a value
+    node(int val)
+    {
+        data = val;
+        left = right = NULL;
     }
 };
 
-int main() {
-    // Creating a new node for the root of the 
-    // binary tree using dynamic allocation
-    Node* root = new Node(1);
-    // Creating left and right child
-    // nodes for the root node
-    root->left = new Node(2);
-    root->right = new Node(3);
-    // Creating a right child node for
-    // the left child node of the root
-    root->left->right = new Node(5);
+int main()
+{   
+    // creating root to the binary tree
+    node* root = new node(1);
+    /*
+        creating the binary tree:
+                1
+               / \
+              2   3
+               \
+                5
+    */
+    root -> left = new node(2);
+    root -> right = new node(3);
+    root -> left -> right = new node(5);
+
+    return 0;
 }
